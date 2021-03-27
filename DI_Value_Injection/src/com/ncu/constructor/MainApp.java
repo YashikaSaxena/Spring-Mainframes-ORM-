1acdde1907636ee7a5607dfef8e117fcd9e7fa65
YashikaSaxena <69866241+YashikaSaxena@users.noreply.github.com>
2021-03-28 00:38:56 +0530

    Update MainApp.java

--- DI_Value_Injection/src/com/ncu/constructor/MainApp.java
+++ DI_Value_Injection/src/com/ncu/constructor/MainApp.java
@@ -11,19 +11,12 @@ public class MainApp {
 		Coach coach=(Coach)context.getBean("ccoach");
 		
 		System.out.println("calling constructor injection");
-		//Coach coach1=(Coach)context.getBean("ccoach1");
+		System.out.println(coach.toString());
+		
 		//System.out.println(coach.getDailyWorkout());
 		//System.out.println(coach.getDailyFortune());
 		//System.out.println(coach.getName());
-		System.out.println(coach.toString());
 		
-		
-		
-		//System.out.println(coach1.getDailyWorkout());
-		//System.out.println(coach1.getDailyFortune());
-		//System.out.println(coach1.getName());
-		//System.out.println(coach1.toString());
-	//System.out.println(coach1.getDailyFortune());
 
 	}
 
