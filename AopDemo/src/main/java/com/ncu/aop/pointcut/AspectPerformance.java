13d06403ae60b5cb1c704d8789b7eaa98f8e723b
YashikaSaxena <69866241+YashikaSaxena@users.noreply.github.com>
2021-05-25 21:55:43 +0530

    Update AspectPerformance.java

--- AopDemo/src/main/java/com/ncu/aop/pointcut/AspectPerformance.java
+++ AopDemo/src/main/java/com/ncu/aop/pointcut/AspectPerformance.java
@@ -12,21 +12,23 @@ public class AspectPerformance {
 long start,end;
 	
 	@Pointcut("execution(* * *Logic())")
-	private void onLogicX() {}
-	
-	@Before("execution(public void logicX())")
-	public void beforeLogicX()
-	{
-		start=System.currentTimeMillis();
-		
-	}
-	
-	@After("execution(public void logicX())")
-	public void afterLogicX()
-	{
-		end=System.currentTimeMillis();
-		long time=end-start;
-		System.out.println("Time taken in execution: "+ time);
-	}
+	public void expression1() {
+
+}
+@Around("expression1()")
+public Object aroundAdvice(ProceedingJoinPoint jp) { Object obj = null;
+long startTime = 0;
+try {
+System.out.println("Now Start Measuring Time"); startTime = System.currentTimeMillis();
+obj = jp.proceed();
+}catch(Throwable e) {
+e.printStackTrace();
+}finally {
+System.out.println("Now End Measuring Time"); long endTime = System.currentTimeMillis();
+System.out.println("Total Time is: "+(endTime - startTime)+" ms");
+}
+return obj;
+}
+
 
 }
